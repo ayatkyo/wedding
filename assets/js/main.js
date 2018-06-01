82d9754fdce1bd1ce8a37343e5ff1d3bfaa20920
Ayat Kyo <ayat.kyo@gmail.com>
2018-06-02 07:35:18 +0800

    Change Parallax, add support for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,15 @@
 function parallaxInit() {
     var $window = $(window);
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    $window.on('scroll resize', function () {
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    });
-
     $('[data-type="background"]').each(function () {
         var $backgroundObj = $(this);
-        var bgOffset = parseInt($backgroundObj.offset().top);
-        var yPos;
-        var coords;
         var speed = ($backgroundObj.data('speed') || 0);
-        if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/))
-            speed = - (speed / 2);
+        var posisiY;
+
         $window.on('scroll resize', function () {
-            yPos = - ((scrollTop - bgOffset) / speed);
-            coords = '35% ' + yPos + 'px';
-            $backgroundObj.css({ backgroundPosition: coords });
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            posisiY = - (scrollTop / speed);
+            $backgroundObj.css({ transform: 'translateY(' + posisiY + 'px)' });
         });
     });
 
